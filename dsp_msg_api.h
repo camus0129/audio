c3a46fde5e1bba2ef5895738e0fc30697af3ab80
Sven <camus0129@live.com>
2024-11-14 16:25:37 +0800

    extern dsp msg api

--- dsp_msg_api.h
+++ dsp_msg_api.h
@@ -3,4 +3,18 @@
 #include "dsp_msg.h"
 void tcl_dsp_init(void);
 void tcl_dsp_sleep(int mode);
+void tcl_dsp_wake_up(int mode);
+void tcl_dsp_input_mix_set(int mode);
+void tcl_dsp_output_mix_set(int mode);
+void tcl_dsp_master_volume_set(int gain);
+void tcl_dsp_channel_volume_set(int channel, int gain);
+void tcl_dsp_mute_set(int state);
+void tcl_dsp_unmute_set(void);
+void tcl_dsp_bass_set(int bass);
+void tcl_dsp_treble_set(int treble);
+void tcl_dsp_channel_test_set(int channel, int gain);
+void tcl_dsp_eq_set(int mode);
+void tcl_dsp_drc_set(int mode);
+void tcl_dsp_agl_set(int mode);
+void tcl_dsp_analog_set(int gain);
 #endif
